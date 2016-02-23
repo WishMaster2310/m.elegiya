562e05f0393da47a5d196784dacbb04c1ef9bde3
wishmaster <wishmaster2310@gmail.com>
2016-02-24 00:15:54 +0500

    add new content

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 
 	$('.j-slider').owlCarousel({
-			 items:1,
-			 nav: true,
-			 navText: ['', ''],
-			 navRewind: false
-		});
+		 items:1,
+		 nav: true,
+		 navText: ['', ''],
+		 navRewind: false
+	});
+
+	$('.header__burger').on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('header__burger--active')
+		$('.menu-container').toggleClass('menu-container--active');
+	})
 
 });
